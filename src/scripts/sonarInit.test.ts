f8bb42c82fffad5d70536c63eac486fad836f59d
Géraud Lac <geraud.lac@ville.montreal.qc.ca>
2021-09-15 17:01:32 -0400

    test: add a second unit test (failing so far)
    Signed-off-by: Géraud Lac <geraud.lac@montreal.ca>

--- src/scripts/sonarInit.test.ts
+++ src/scripts/sonarInit.test.ts
@@ -3,10 +3,12 @@
 // tslint:disable:no-console
 // ==========================================
 import { describe, it } from 'mocha';
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 import { setTestingConfigs, timeout } from '../utils/testingUtils';
 import { SonarInitScript } from './sonarInit';
 import * as sinon from 'sinon';
+import * as fs from 'fs-extra';
+const nock = require('nock');
 
 const chai = require('chai');
 chai.use(require('chai-as-promised'));
@@ -45,7 +47,7 @@ class LoggerRecorder {
   }
 }
 
-describe('Test sonar-init script', function() {
+describe.only('Test sonar-init script', function() {
   timeout(this, 30000);
 
   before(() => {
@@ -65,4 +67,46 @@ describe('Test sonar-init script', function() {
 error: Script "sonar-init" failed after 0 s with: ENOENT: no such file or directory, open 'sonar-project.properties'
 `);
   });
+
+  describe(' with valid sonar-project.properties file', async () => {
+    before(async () => {
+      await fs.copyFile('./src/utils/test-sonar-project.properties', './sonar-project.properties');
+    });
+    after(async () => {
+      await fs.unlink('./sonar-project.properties');
+    });
+
+    afterEach(() => {
+      nock.cleanAll();
+    });
+
+    it(` should do something`, async () => {
+      nock('https://example.com')
+      .get('/sonar/api/project_branches/list')
+      .query({ project: 'my-test-project-key' })
+      .reply(200);
+
+      const loggerRecorder = new LoggerRecorder();
+      const sonarInitScript = getSonarInitScript(false, loggerRecorder.logger);
+
+      console.log('***** Launching sonar-init script in unit test *****');
+
+      await sonarInitScript.run();
+
+      // TODO Geraud : NOT YET IMPLEMENTED
+
+      console.log('***** Pending mocks *****');
+      console.log(nock.pendingMocks());
+      console.log('*************************');
+
+      assert.isTrue(nock.isDone(), `There are remaining expected HTTP calls: ${nock.pendingMocks().toString()}`);
+
+      const expectedOutput = `info: Script "sonar-init" starting...
+info: Initializing 'my-test-project-key' Sonar project...
+Blah Blah Blah
+`;
+      expect(loggerRecorder.recordedLogs).to.equal(expectedOutput);
+    });
+  });
+
 });
